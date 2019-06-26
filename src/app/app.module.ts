725af547e27c5e8642f6d83f6c83ac890cebe038
Sergey Afanasev <sergey_afanasyev@yahoo.com>
2019-06-25 22:50:49 -0400

    photo_album_v1.0

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FlexLayoutModule } from '@angular/flex-layout';
+
+import { MatCardModule } from '@angular/material/card';
+import { environment } from '../environments/environment';
+import { FileService } from './service/file.service';
+import { FileExplorerModule } from './file-explorer/file-explorer.module';
+import { PhotoComponent } from './photo/photo.component';
+import { HttpModule } from '@angular/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PhotoComponent
   ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
+  imports: [BrowserModule, FileExplorerModule, FlexLayoutModule, MatCardModule, HttpModule],
+  providers: [FileService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/service/file.service.ts
@@ -0,0 +1,115 @@
+import { Injectable } from '@angular/core';
+
+import { v4 } from 'uuid';
+import { FileElement } from '../file-explorer/model/element';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+import { Observable } from 'rxjs/Observable';
+import { Headers, Http } from '@angular/http';
+
+export interface IFileService {
+  add(fileElement: FileElement);
+  delete(id: string);
+  update(id: string, update: Partial<FileElement>);
+  queryInFolder(folderId: string): Observable<FileElement[]>;
+  get(id: string): FileElement;
+}
+
+@Injectable()
+export class FileService implements IFileService {
+  private folderMap = new Map<string, FileElement>();
+  private serviceUrl = '/api/photo';  // URL to
+  private querySubject: BehaviorSubject<FileElement[]>;
+
+  private handleError(error: any): Promise<any> {
+    console.error('An error occurred', error); // for demo purposes only
+    return Promise.reject(error.message || error);
+  }
+
+
+  constructor(private http: Http) {}
+
+  add(fileElement: FileElement) {
+    fileElement.id = v4();
+    this.folderMap.set(fileElement.id, this.clone(fileElement));
+    return fileElement;
+  }
+
+  delete(id: string) {
+    this.folderMap.delete(id);
+  }
+
+  update(id: string, update: Partial<FileElement>) {
+    let element = this.folderMap.get(id);
+    element = Object.assign(element, update);
+    this.folderMap.set(element.id, element);
+  }
+
+//not in use
+  queryInFolder(folderId: string) {
+    const result: FileElement[] = [];
+    this.folderMap.forEach(element => {
+      if (element.parent === folderId) {
+        result.push(this.clone(element));
+      }
+    });
+    if (!this.querySubject) {
+      this.querySubject = new BehaviorSubject(result);
+    } else {
+      this.querySubject.next(result);
+    }
+    return this.querySubject.asObservable();
+  }
+  ////////////////////////////////////
+
+  queryFolderMap() {
+    const result: FileElement[] = [];
+    this.folderMap.forEach(element => {
+        result.push(this.clone(element));
+    });
+    if (!this.querySubject) {
+      this.querySubject = new BehaviorSubject(result);
+    } else {
+      this.querySubject.next(result);
+    }
+    return this.querySubject.asObservable();
+  }
+  _get(path): Promise<any> {
+    return this.http.post(this.serviceUrl,{
+      body: path
+    })
+    .toPromise()
+    .then((response:any) => {
+       this.folderMap = new Map<string, FileElement>();
+       //console.log('response.data', response);
+       let body=JSON.parse(response.text());
+       if (body.status && body.status=='ok' && body.data) {
+         body.data.map((item:any) =>{
+           this.add({ name: item.name, isFolder: item.isFolder, parent: 'root' })
+         });
+         //console.log('folderMap', this.folderMap);
+       }
+       return this.folderMap;
+     })
+     .catch(this.handleError);
+  }
+  _getURL(path) {
+    return this.serviceUrl+path;
+  }
+  renderFile(path):Promise<any> {
+    return this.http.post(this.serviceUrl,{
+      body: path
+    })
+    .toPromise()
+    .then((response:any) => {
+        return response;
+    })
+    .catch(this.handleError);
+  }
+  get(id: string) {
+    return this.folderMap.get(id);
+  }
+
+  clone(element: FileElement) {
+    return JSON.parse(JSON.stringify(element));
+  }
+}
